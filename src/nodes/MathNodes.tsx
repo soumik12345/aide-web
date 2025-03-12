1e846daad56f38f332a9540036c1b6aef3203407
soumik12345 <19soumik.rakshit96@gmail.com>
2025-03-12 10:57:06 -0700

    update: math nodes input sockets

--- src/nodes/MathNodes.tsx
+++ src/nodes/MathNodes.tsx
@@ -5,9 +5,7 @@ export function AddNode({ data }: NodeProps<AddNode>) {
   return (
     <div className="react-flow__node-default">
       <div>{data.label || 'Add'}</div>
-      <Handle type="target" position={Position.Top} />
-      <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Bottom} />
+      <Handle type="source" position={Position.Left} />
     </div>
   );
 }
@@ -16,9 +14,7 @@ export function SubtractNode({ data }: NodeProps<SubtractNode>) {
   return (
     <div className="react-flow__node-default">
       <div>{data.label || 'Subtract'}</div>
-      <Handle type="target" position={Position.Top} />
-      <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Bottom} />
+      <Handle type="source" position={Position.Left} />
     </div>
   );
 }
@@ -27,9 +23,7 @@ export function MultiplyNode({ data }: NodeProps<MultiplyNode>) {
   return (
     <div className="react-flow__node-default">
       <div>{data.label || 'Multiply'}</div>
-      <Handle type="target" position={Position.Top} />
-      <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Bottom} />
+      <Handle type="source" position={Position.Left} />
     </div>
   );
 }
@@ -38,9 +32,7 @@ export function DivideNode({ data }: NodeProps<DivideNode>) {
   return (
     <div className="react-flow__node-default">
       <div>{data.label || 'Divide'}</div>
-      <Handle type="target" position={Position.Top} />
-      <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Bottom} />
+      <Handle type="source" position={Position.Left} />
     </div>
   );
 } 
